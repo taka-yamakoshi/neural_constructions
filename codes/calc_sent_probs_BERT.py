4f9fd7c4c1782678a357dbb9bc8ed729f39de426
山越貴耀 <takateru@nat-oitwireless-inside-vapornet100-10-9-90-72.princeton.edu>
2020-01-29 17:56:21 -0500

    added indef_art and len

---
+++ codes/calc_sent_probs_BERT.py
@@ -0,0 +1,52 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from transformers import BertTokenizer, BertModel, BertForMaskedLM
+import sys
+
+sys.path.append('..')
+args = sys.argv
+with open("mypath.txt") as f:
+    PATH = f.read()
+
+def softmax(x):
+    return torch.exp(x)/torch.sum(torch.exp(x))
+
+#Load sentences
+sentence_file = open(PATH+'textfile/'+ args[1] + '_' + args[2] +'.txt')
+sentence_str = sentence_file.read()
+sentence_file.close()
+sentence_str = sentence_str.split('\n')[:-1]
+
+
+#Load the model
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+model.eval()
+
+
+prob_list = []
+
+for sentence in sentence_str:
+    tokenized_text = sentence.split(" ")
+    sent_prob = 0
+    sent_len = len(sentence.split(" "))
+    for masked_index in range(3,sent_len):
+        masked_text = tokenized_text.copy()
+        masked_text[masked_index] = '[MASK]'
+        trunc_text = masked_text[:masked_index+1]
+        indexed_tokens = tokenizer.convert_tokens_to_ids(trunc_text)
+        tokens_tensor = torch.tensor([indexed_tokens])
+        with torch.no_grad():
+            outputs = model(tokens_tensor)
+            predictions = outputs[0]
+        probs = predictions[0, masked_index]
+        log_probs = torch.log(softmax(probs))
+        prob = log_probs[tokenizer.convert_tokens_to_ids(tokenized_text[masked_index])].item()
+    sent_prob += prob
+    prob_list.append(sent_prob)
+
+#Save the data
+with open(PATH + 'datafile/sent_log_probs_' + args[1] + '_' + args[2] + '.pkl','wb') as f:
+    pickle.dump(np.array(prob_list),f)
---
+++ codes/plot_probs_indef_art.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import sys
+
+sys.path.append('..')
+args = sys.argv
+with open("mypath.txt") as f:
+    PATH = f.read()
+
+with open(PATH + 'datafile/sent_log_probs_good_do_bert.pkl','rb') as f:
+    probs_pron_do = pickle.load(f)
+with open(PATH + 'datafile/sent_log_probs_good_po_bert.pkl','rb') as f:
+    probs_pron_po = pickle.load(f)
+with open(PATH + 'datafile/sent_log_probs_good_do_indef_art.pkl','rb') as f:
+    probs_indef_do = pickle.load(f)
+with open(PATH + 'datafile/sent_log_probs_good_po_indef_art.pkl','rb') as f:
+    probs_indef_po = pickle.load(f)
+
+def calculate_t(x,y):
+    x_ave = np.average(x)
+    y_ave = np.average(y)
+    x_var = np.var(x)
+    y_var = np.var(y)
+    pooled_var = (x_var*(x.size-1)+y_var*(y.size-1))/(x.size+y.size-2)
+    return (x_ave-y_ave)/(np.sqrt(pooled_var*(1/x.size+ 1/y.size)))
+
+
+pron_log_ratio = [probs_pron_do[j] - probs_pron_po[j] for j in range(len(probs_pron_do))]
+indef_log_ratio = [probs_indef_do[j] - probs_indef_po[j] for j in range(len(probs_indef_do))]
+print("t = " + str(calculate_t(np.array(pron_log_ratio),np.array(indef_log_ratio))))
+
+fig, axis = plt.subplots()
+probs = [pron_log_ratio,indef_log_ratio]
+plt.bar(np.arange(2),np.array([np.average(probs[i]) for i in range(2)]),yerr =np.array([np.std(probs[i])/np.sqrt(len(probs[i])) for i in range(2)]))
+plt.xticks([0,1],["Pronouns","Nouns with indefinite articles"])
+axis.set_xlabel("Types of objects")
+axis.set_ylabel("Log likelihood ratio")
+plt.gca().invert_yaxis()
+plt.show()
+
