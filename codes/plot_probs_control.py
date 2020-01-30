61d4d4abc1515c8e3ad2af126f7d7e7f7b782df5
山越貴耀 <takateru@nat-oitwireless-inside-vapornet100-10-9-90-72.princeton.edu>
2020-01-29 21:57:45 -0500

    done with garden

--- codes/plot_probs_control.py
+++ codes/plot_probs_control.py
@@ -12,10 +12,10 @@ with open(PATH + 'datafile/sent_log_probs_good_do_bert.pkl','rb') as f:
     probs_pron_do = pickle.load(f)
 with open(PATH + 'datafile/sent_log_probs_good_po_bert.pkl','rb') as f:
     probs_pron_po = pickle.load(f)
-with open(PATH + 'datafile/sent_log_probs_good_do_indef_art.pkl','rb') as f:
-    probs_indef_do = pickle.load(f)
-with open(PATH + 'datafile/sent_log_probs_good_po_indef_art.pkl','rb') as f:
-    probs_indef_po = pickle.load(f)
+with open(PATH + 'datafile/sent_log_probs_good_do_'+args[1]+'.pkl','rb') as f:
+    probs_control_do = pickle.load(f)
+with open(PATH + 'datafile/sent_log_probs_good_po_'+args[1]+'.pkl','rb') as f:
+    probs_control_po = pickle.load(f)
 
 def calculate_t(x,y):
     x_ave = np.average(x)
@@ -27,13 +27,13 @@ def calculate_t(x,y):
 
 
 pron_log_ratio = [probs_pron_do[j] - probs_pron_po[j] for j in range(len(probs_pron_do))]
-indef_log_ratio = [probs_indef_do[j] - probs_indef_po[j] for j in range(len(probs_indef_do))]
-print("t = " + str(calculate_t(np.array(pron_log_ratio),np.array(indef_log_ratio))))
+control_log_ratio = [probs_control_do[j] - probs_control_po[j] for j in range(len(probs_control_do))]
+print("t = " + str(calculate_t(np.array(pron_log_ratio),np.array(control_log_ratio))))
 
 fig, axis = plt.subplots()
-probs = [pron_log_ratio,indef_log_ratio]
+probs = [pron_log_ratio,control_log_ratio]
 plt.bar(np.arange(2),np.array([np.average(probs[i]) for i in range(2)]),yerr =np.array([np.std(probs[i])/np.sqrt(len(probs[i])) for i in range(2)]))
-plt.xticks([0,1],["Pronouns","Nouns with indefinite articles"])
+plt.xticks([0,1],["Pronouns","Control"])
 axis.set_xlabel("Types of objects")
 axis.set_ylabel("Log likelihood ratio")
 plt.gca().invert_yaxis()
