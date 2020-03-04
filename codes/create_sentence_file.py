1e1966b5267b623069ca6a5c97e4cbbb25e533e2
山越貴耀 <takateru@nat-oitwireless-inside-vapornet100-10-9-90-72.princeton.edu>
2020-03-03 19:43:28 -0500

    edited sentence generation

--- codes/create_sentence_file.py
+++ codes/create_sentence_file.py
@@ -15,6 +15,8 @@ verb_theme_pairs_bad = file.read().split('\n')[:-1]
 file.close()
 
 pairs = [verb_theme_pairs_good,verb_theme_pairs_bad]
+recipients = ["him","her","the man", "the woman","a man","a woman","the man from childhood","the woman from childhood","a man from childhood","a woman from childhood"]
+names = ["mohamed","sara","liam","maria","ali","adam","lucas","emma","oliver","olivia"]
 
 verb_type = ['good','bad']
 type = ['do','pd']
@@ -23,31 +25,22 @@ for i in range(2):
         with open(PATH +'textfile/'+ verb_type[i] + '_'+type[j]+'test.txt','w') as f:
             if j == 0:
                 for pair in pairs[i]:
-                    f.write("[CLS] the man ")
-                    f.write(pair.split(" ")[0])
-                    f.write(" her the ")
-                    f.write(pair.split(" ")[1])
-                    f.write(" . [SEP]")
-                    f.writelines('\n')
-                    f.write("[CLS] the woman ")
-                    f.write(pair.split(" ")[0])
-                    f.write(" him the ")
-                    f.write(pair.split(" ")[1])
-                    f.write(" . [SEP]")
-                    f.writelines('\n')
+                    for recipient in recipients:
+                        for subj in names:
+                            f.write("[CLS] "+subj+" ")
+                            f.write(pair.split(" ")[0])
+                            f.write(" "+recipient+" the ")
+                            f.write(pair.split(" ")[1])
+                            f.write(" . [SEP]")
+                            f.writelines('\n')
             if j == 1:
                 for pair in pairs[i]:
-                    f.write("[CLS] the man ")
-                    f.write(pair.split(" ")[0])
-                    f.write(" the ")
-                    f.write(pair.split(" ")[1])
-                    f.write(" to ")
-                    f.write("her . [SEP]")
-                    f.writelines('\n')
-                    f.write("[CLS] the woman ")
-                    f.write(pair.split(" ")[0])
-                    f.write(" the ")
-                    f.write(pair.split(" ")[1])
-                    f.write(" to ")
-                    f.write("him . [SEP]")
-                    f.writelines('\n')
+                    for recipient in recipients:
+                        for subj in names:
+                            f.write("[CLS] "+subj+" ")
+                            f.write(pair.split(" ")[0])
+                            f.write(" the ")
+                            f.write(pair.split(" ")[1])
+                            f.write(" to ")
+                            f.write(recipient + " . [SEP]")
+                            f.writelines('\n')
