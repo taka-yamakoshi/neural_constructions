a32cfdb3706ee2106d68034ae4300ea9483f7903
山越貴耀 <takateru@yamakoshinoMacBook-Pro.local>
2020-04-30 03:44:23 +0900

    organized code files

--- codes/calculate_sent_probs_new.py
+++ codes/calculate_sent_probs_new.py
@@ -38,9 +38,7 @@ def calc_sent_prob(sentence):
 with open(PATH+'textfile/generated_pairs.csv') as f:
     reader = csv.reader(f)
     corpus = [row for row in reader][1:]
-
 sent_list = []
-
 for pair in corpus:
     if pair[-1] == args[1]:
         sent_list.append([pair[4],pair[5]])
