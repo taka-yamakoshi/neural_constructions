13d690867ccae2dd60b14b3f697793db1e87c038
山越貴耀 <takateru@yamakoshinoMacBook-Pro.local>
2020-04-30 03:44:23 +0900

    organized code files

--- codes/plot_performance.py
+++ codes/plot_performance.py
@@ -14,6 +14,10 @@ with open(PATH + 'datafile/test_performance.pkl','rb') as f:
     test_performance = pickle.load(f)
 
 plt.imshow(train_performance)
+plt.xlabel("Head Number")
+plt.ylabel("Layer Number")
 plt.show()
 plt.imshow(test_performance)
+plt.xlabel("Head Number")
+plt.ylabel("Layer Number")
 plt.show()
