ed13769d76c445e32793db7361acba55b691cc3a
山越貴耀 <takateru@yamakoshinoMacBook-Pro.local>
2020-05-06 11:28:47 +0900

    minor modification of attention analysis

--- codes/ExtractAttention.py
+++ codes/ExtractAttention.py
@@ -5,14 +5,44 @@ import matplotlib.pyplot as plt
 import torch
 from transformers import BertTokenizer, BertModel, BertForMaskedLM
 import sys
+import logging
 
 sys.path.append('..')
 args = sys.argv
+##First argument: construction type (DO or PD)
+##Second arguemnt: recipient type (shortDefinite only at this moment)
+##Third argument: default sentence length (8 for DO, 9 for PD)
 with open("mypath.txt") as f:
     PATH = f.read()
 
 def softmax(x):
     return torch.exp(x)/torch.sum(torch.exp(x))
+
+def shrink_attention(sent_type,theme_num,raw_attn):
+    if sent_type == "DO":
+        added_attn = np.zeros((12,len(tokenized_sentence),int(args[3])))
+        added_attn[:,:,:5] = raw_attn[:,:,:5]
+        added_attn[:,:,-2:] = raw_attn[:,:,-2:]
+        added_attn[:,:,5] = np.sum(raw_attn[:,:,5:(5+theme_num)],axis=2)
+        shrunk_attn = np.zeros((12,int(args[3]),int(args[3])))
+        shrunk_attn[:,:5,:] = added_attn[:,:5,:]
+        shrunk_attn[:,-2:,:] = added_attn[:,-2:,:]
+        shrunk_attn[:,5,:] = np.average(added_attn[:,5:(5+theme_num),:],axis=1)
+    elif sent_type == "PD":
+        added_attn = np.zeros((12,len(tokenized_sentence),int(args[3])))
+        added_attn[:,:,:3] = raw_attn[:,:,:3]
+        added_attn[:,:,-5:] = raw_attn[:,:,-5:]
+        added_attn[:,:,3] = np.sum(raw_attn[:,:,3:(3+theme_num)],axis=2)
+        shrunk_attn = np.zeros((12,int(args[3]),int(args[3])))
+        shrunk_attn[:,:3,:] = added_attn[:,:3,:]
+        shrunk_attn[:,-5:,:] = added_attn[:,-5:,:]
+        shrunk_attn[:,3,:] = np.average(added_attn[:,3:(3+theme_num),:],axis=1)
+    else:
+        logging.error('Invalid construction type')
+    return shrunk_attn
+
+def extract_theme_attn(sent_type,attn):
+
 
 #Load sentences
 with open(PATH+'textfile/generated_pairs.csv') as f:
@@ -30,28 +60,32 @@ model.eval()
 
 const_type = ["DO","PD"]
 
-##TODO: Account for varying length of tokenized_sentence##
+##Only works for shortDefinite recipients##
 attention_list = []
 mask_id = tokenizer.encode("[MASK]")[1:-1][0]
-for i, verb in enumerate(verb_list):
-    verb_id = tokenizer.encode(verb)[1:-1]
-    if len(verb_id) == 1:
-        for sentence in sent_list[5*i:5*(i+1)]:
-            words = sentence[const_type.index(args[1])].split(" ")
-            words.append(".")
-            tokenized_sentence = tokenizer.encode(" ".join(words))
-            if len(tokenized_sentence) == 8:
-                verb_pos = tokenized_sentence.index(verb_id[0])
-                tokenized_sentence[verb_pos] = mask_id
-                input_tensor = torch.tensor([tokenized_sentence])
-                with torch.no_grad():
-                    outputs = model(input_tensor)
-                    attention = []
-                    for layer in outputs[2]:
-                        attention.append(list(layer[0].numpy()))
-                    attention_list.append(np.array(attention))
+for sentence in sent_list:
+    words = sentence[const_type.index(args[1])].split(" ")
+    words.append(".")
+    words[1] = "[MASK]"
+    tokenized_sentence = tokenizer.encode(" ".join(words))
+    input_tensor = torch.tensor([tokenized_sentence])
+    with torch.no_grad():
+        outputs = model(input_tensor)
+        attention = []
+        ##shrink the attention map as appropriate for each condition##
+        if len(tokenized_sentence) == int(args[3]):
+            for layer in outputs[2]:
+                attention.append(list(layer[0].numpy()))
+            attention_list.append(np.array(attention))
+        elif len(tokenized_sentence) > int(args[3]):
+            theme_num = len(tokenized_sentence) - int(args[3]) + 1
+            for layer in outputs[2]:
+                raw_attn = layer[0].numpy()
+                shrunk_attn = shrink_attention(args[1],theme_num,raw_attn)
+                attention.append(list(shrunk_attn))
+            attention_list.append(np.array(attention))
 ave_attention = np.average(np.array(attention_list),axis = 0)
-with open(PATH+'datafile/attention_map.pkl','wb') as f:
+with open(PATH+'datafile/attention_map_'+args[1]+'_'+args[2]+'.pkl','wb') as f:
     pickle.dump(ave_attention,f)
 fig, axis = plt.subplots(12,12)
 for i in range(12):
