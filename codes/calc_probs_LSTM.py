b0d913c03d62401bae4c37e5ff23ef4da892f969
山越貴耀 <takateru@yamakoshinoMacBook-Pro.local>
2020-05-06 12:30:29 +0900

    modified LSTM baseline

--- codes/calc_probs_LSTM.py
+++ codes/calc_probs_LSTM.py
@@ -1,12 +1,16 @@
 import pickle
 import numpy as np
 import torch
+import csv
+import logging
 import model
 import matplotlib.pyplot as plt
 import sys
 
 sys.path.append('..')
 args = sys.argv
+##First argument: comparison categery (recipient or theme)
+##Second argument: recipient/theme type
 with open("mypath.txt") as f:
     PATH = f.read()
 
@@ -36,16 +40,28 @@ with open(PATH+'datafile/word2id.pkl', 'rb') as f:
 with open(PATH+'datafile/id2word.pkl', 'rb') as f:
     id2word = pickle.load(f)
 
+
 #Load sentences
-sentence_file = open(PATH+'textfile/'+ args[1] + '_' + args[2] +'.txt')
-sentence_str = sentence_file.read()
-sentence_file.close()
+with open(PATH+'textfile/generated_pairs.csv') as f:
+    reader = csv.reader(f)
+    file = [row for row in reader]
+    head = file[0]
+    corpus = file[1:]
+    if args[1] == "recipient":
+        sent_list_alt = [[pair[head.index('DOsentence')],pair[head.index('PDsentence')]] for pair in corpus if pair[head.index('recipient_id')] == args[2] and pair[head.index('classification')] == "alternating"]
+        sent_list_nonalt = [[pair[head.index('DOsentence')],pair[head.index('PDsentence')]] for pair in corpus if pair[head.index('recipient_id')] == args[2] and pair[head.index('classification')] == "non-alternating"]
+    elif args[1] == "theme":
+        sent_list_alt = [[pair[head.index('DOsentence')],pair[head.index('PDsentence')]] for pair in corpus if pair[head.index('theme_type')] == args[2] and pair[head.index('classification')] == "alternating"]
+        sent_list_nonalt = [[pair[head.index('DOsentence')],pair[head.index('PDsentence')]] for pair in corpus if pair[head.index('theme_type')] == args[2] and pair[head.index('classification')] == "non-alternating"]
+    else:
+        logging.error('Invalid comparison category: should be either "recipient" or "theme"')
 
-sentence_str = sentence_str.split('\n')[:-1]
-sentence_str = [sentence.split(" ") for sentence in sentence_str]
-sentence_data = [torch.tensor([word2id[word] if word in word2id else word2id['<unk>'] for word in sentence]) for sentence in sentence_str]
+sentence_data_DO_alt = [torch.tensor([word2id[word] if word in word2id else word2id['<unk>'] for word in sentence[0].split(" ")]) for sentence in sent_list_alt]
+sentence_data_PD_alt = [torch.tensor([word2id[word] if word in word2id else word2id['<unk>'] for word in sentence[1].split(" ")]) for sentence in sent_list_alt]
+sentence_data_DO_nonalt = [torch.tensor([word2id[word] if word in word2id else word2id['<unk>'] for word in sentence[0].split(" ")]) for sentence in sent_list_nonalt]
+sentence_data_PD_nonalt = [torch.tensor([word2id[word] if word in word2id else word2id['<unk>'] for word in sentence[1].split(" ")]) for sentence in sent_list_nonalt]
 
-# LSTM model
+#LSTM model
 class trained_model():
     def __init__(self,W_in, W_x0, W_h0, b_x0, b_h0, W_x1, W_h1, b_x1, b_h1, W_out, b_out, word2id,id2word):
         return None
@@ -102,17 +118,26 @@ init_h0 = torch.zeros(650)
 init_c1 = torch.zeros(650)
 init_h1 = torch.zeros(650)
 
+def calculate_probs(sentence_data):
+    hid_state = [init_c0,init_h0,init_c1,init_h1]
+    total_probs = []
+    seed = 2020
+    np.random.seed(seed)
+    random_seq = np.random.permutation(len(sentence_data))
+    random_sentence_data = [sentence_data[random_seq[j]] for j in range(len(random_seq))]
+    for i,sentence in enumerate(random_sentence_data):
+        hid_state, probs =  my_model.forward(sentence,hid_state)
+        total_probs.append(probs)
+        if i%100 == 0:
+            print(str(i) + " sentences done")
+    return total_probs
 
-hid_state = [init_c0,init_h0,init_c1,init_h1]
-total_probs = []
-seed = 2020
-np.random.seed(seed)
-random_seq = np.random.permutation(len(sentence_data))
-random_sentence_data = [sentence_data[random_seq[j]] for j in range(len(random_seq))]
-for i,sentence in enumerate(random_sentence_data):
-    hid_state, probs =  my_model.forward(sentence,hid_state)
-    total_probs.append(probs)
-
+DO_alt_log_prob = calculate_probs(sentence_data_DO_alt)
+PD_alt_log_prob = calculate_probs(sentence_data_PD_alt)
+DO_nonalt_log_prob = calculate_probs(sentence_data_DO_nonalt)
+PD_nonalt_log_prob = calculate_probs(sentence_data_PD_nonalt)
+alt_log_ratio = np.array(DO_alt_log_prob) - np.array(PD_alt_log_prob)
+nonalt_log_ratio = np.array(DO_nonalt_log_prob) - np.array(PD_nonalt_log_prob)
 #Save the data
-with open(PATH + 'datafile/log_probs_'+args[1]+'_'+args[2]+'.pkl','wb') as f:
-    pickle.dump(total_probs,f)
+with open(PATH + 'datafile/lstm_log_ratio_'+args[2]+'.pkl','wb') as f:
+    pickle.dump([alt_log_ratio,nonalt_log_ratio],f)
