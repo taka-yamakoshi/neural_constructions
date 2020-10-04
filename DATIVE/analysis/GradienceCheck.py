3bb15a7b239678a8cacfdeaa9a9e9929ecd3401b
Robert Hawkins <rxdh@stanford.edu>
2020-10-03 23:35:22 -0700

    tune up gradience check plot

--- DATIVE/analysis/GradienceCheck.py
+++ DATIVE/analysis/GradienceCheck.py
@@ -17,12 +17,13 @@ verb_data = pd.read_csv('../data/experiment_input/verblists.csv')
 verb_list = [list(verb_data.query(f'verb_id == {id+1}')['verb'])[0] for id in range(200)]
 
 color_list = sns.color_palette('Set2')
-fig = plt.figure(figsize=(30,15),dpi=150)
-ymax_list = [40,30,30,20,20,20,20,20,20,50]
+fig = plt.figure(figsize=(15.5, 20),dpi=150)
+ymax_list = [40,30,30,20,20,20,20,20,20,20,
+             20,20,20,20,20,20,20,25,20,45]
 for pos,id in enumerate(sorted_verb_id):
-    ax1 = fig.add_subplot(10,20,pos+1)
+    ax1 = fig.add_subplot(17,12,pos+1)
     ax2 = ax1.twinx()
-    kde_model = gaussian_kde(behav_data_pronoun[id],bw_method=0.5)
+    kde_model = gaussian_kde(behav_data_pronoun[id],bw_method='scott')
     x_grid = np.linspace(0,101,num=500)
     if id < 100:
         ax1.hist(behav_data_pronoun[id],color=color_list[1],alpha=0.5,bins=np.arange(0,101,10))
@@ -32,24 +33,20 @@ for pos,id in enumerate(sorted_verb_id):
         ax2.plot(x_grid,kde_model(x_grid), color=color_list[0],linewidth=2)
 
     ax1.set_xticks([0,50,100])
-    if pos < 180:
+    if pos < 188:
         ax1.set_xticklabels([])
     else:
         ax1.set_xticklabels([0,50,100],fontsize=8,rotation=90)
-    ymax = ymax_list[pos//20]
+    ymax = ymax_list[pos//12]
     ax1.set_ylim(0,ymax)
-    if pos%20==0:
+    if pos % 12==0:
         ax1.set_yticks([0,int(ymax/2),ymax])
         ax1.set_yticklabels([0,int(ymax/2),ymax])
     else:
         ax1.set_yticks([])
     ax2.set_ylim([0,0.05])
-    if pos%20==19:
-        ax2.set_yticks([0,0.025,0.05])
-        ax2.set_yticklabels([0,0.025,0.05])
-    else:
-        ax2.set_yticks([])
-    plt.title(verb_list[id],fontsize=12)
+    ax2.set_yticks([])
+    plt.title(verb_list[id],fontsize=13)
     ax1.spines['top'].set_visible(False)
     ax1.spines['right'].set_visible(False)
     ax1.spines['left'].set_visible(False)
@@ -58,4 +55,4 @@ for pos,id in enumerate(sorted_verb_id):
     ax2.spines['left'].set_visible(False)
     ax2.spines['bottom'].set_visible(False)
 fig.subplots_adjust(hspace=0.75,bottom=0.05,top=0.95,left=0.05,right=0.95)
-fig.savefig('../raw_dist.png')
+fig.savefig('../raw_dist.pdf')
